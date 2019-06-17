4dabdd94eee1d6614e1c1687961305c4728706fa
someone <someone@someplace.com>
2019-06-17 18:46:19 +0700

    push

---
+++ wp-content/themes/doanhnghiep/footer.php
@@ -0,0 +1,154 @@
+<div class="scrolltop">
+	<i class="fa fa-angle-up" aria-hidden="true"></i>	
+</div>
+<footer class="footer">
+	<div class="container">
+		<div class="host_patronize">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="host_ft">
+						<h4>CHỦ TRÌ VÀ CHỈ ĐẠO</h4>
+						<ul>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/chutri1.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/chutri2.jpg"></figure></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-9">
+					<div class="patronize">
+						<h4>BẢO TRỢ THÔNG TIN</h4>
+						<ul>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro1.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro2.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro3.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro14.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro5.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro6.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro7.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro8.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro9.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro10.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro11.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro12.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro13.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro14.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro15.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/baotro16.jpg"></figure></li>
+							<li><figure><img src="<?php echo BASE_URL; ?>/images/baotro17.jpg">
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="dvi_dtnn">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="dvi">
+							<h4>ĐƠN VỊ TỔ CHỨC</h4>
+							<ul>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/dvtochuc1.jpg"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/dvtochuc2.jpg"></figure></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-9">
+						<div class="dtnn">
+							<h4>ĐỐI TÁC NƯỚC NGOÀI & ĐỒNG TỔ CHỨC</h4>
+							<ul>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac1.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac2.gif"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac3.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac4.jpg"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac5.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac6.jpg"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac7.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac8.gif"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac9.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac10.gif"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac11.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac12.jpg"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac13.gif"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac14.jpg"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac15.gif"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac16.gif"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac17.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac18.jpg"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac19.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac20.gif"></figure></li>
+								<li><figure><img src="<?php echo BASE_URL; ?>/images/doitac21.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/doitac22.jpg"></figure></li>
+							</ul>
+						</div>
+					</div>
+				</div>		
+			</div>
+			<div class="dvi_join">
+				<h4>ĐỐI TÁC NƯỚC NGOÀI & ĐỒNG TỔ CHỨC</h4>
+				<ul>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg1.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg3.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg5.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg7.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg9.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg11.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg13.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg15.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg17.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg19.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg21.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg23.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg25.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+					<li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg27.jpg"></figure></li><li><figure><img src="<?php echo BASE_URL; ?>/images/donvitg2.jpg"></figure></li>
+				</ul>
+			</div>
+			<div class="info_social">
+				<div class="info_ft">
+					<h3>Công ty Cổ phần Quảng cáo và Hội chợ Thương mại VINEXAD </h3>
+					<ul>
+						<?php if(get_option('address_ft')){ ?>
+							<li>
+								<?php if(get_locale() == 'en_US'){ echo get_option('address_ft_en'); } else { echo get_option('address_ft'); }?>
+							</li>
+						<?php } ?>
+						<?php if(get_option('phone_ft')){ ?>
+							<li>Tel: <?php echo get_option('phone_ft'); ?> </li>
+						<?php } ?>
+						<?php if(get_option('fax_ft')){ ?>
+							<li>Fax: <?php echo get_option('fax_ft'); ?> </li>
+						<?php } ?>
+						<?php if(get_option('email_ft')){ ?>
+							<li>Email: <?php echo get_option('email_ft'); ?></li>
+						<?php } ?>
+					</ul>
+				</div>
+				<div class="social_ft">
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					</ul>
+					<p>Xem bản đồ</p>
+				</div>
+			</div>
+
+			<div class="copyright">
+				<p>©2019 VIETNAM MEDI-PHARM EXPO</p>
+			</div>
+			<div class="popup popup_map">
+				<div class="content_popup">
+					<div class="wrap_content_p">
+						<h2>Bản đồ vị trí hội chợ</h2>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.0402203819053!2d106.7189652141822!3d10.731381362959446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f884b977323%3A0x12bab2d8a70949ca!2zVHJ1bmcgdMOibSBI4buZaSBjaOG7oyB2w6AgVHJp4buDbiBsw6NtIFPDoGkgR8Oybg!5e0!3m2!1svi!2s!4v1513504420367"  frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+					<div class="close_popup"><i class="fa fa-times" aria-hidden="true"></i></div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
+	<!-- MESSENGER -->
+	<script>      
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId      : '1953938748210615',
+				xfbml      : true,
+				version    : 'v2.6'
+			});
+		};
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));      
+	</script>
+	<!-- END  MESSENGER -->
+	<?php get_template_part('order_fixedf'); ?>
+	<script src="<?php echo BASE_URL; ?>/js/wow.min.js"></script>
+	<script src="<?php echo BASE_URL; ?>/js/slick.js"></script>
+	<script src="<?php echo BASE_URL; ?>/js/custom.js"></script>
+</body>
+</html>
